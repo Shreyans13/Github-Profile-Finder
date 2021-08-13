2866dc1e6af97717bb3ca07d2a553b1653aa16d2
Shreyans_13 <shreyans.jain@juspay.in>
2021-08-13 09:16:59 +0530

    updated/ui/followersTab

--- lib/pages/tabs/followers.dart
+++ lib/pages/tabs/followers.dart
@@ -24,70 +24,35 @@ class Followers extends StatelessWidget {
           padding: const EdgeInsets.all(8),
           itemCount: 30,
           itemBuilder: (BuildContext context, int index) {
-            return Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  width: 1.3,
-                  color: colorCodes[index % 3],
-                ),
-                color: colorCodes[index % 3],
+            return ListTile(
+              tileColor: colorCodes[index % 3],
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/50544190?v=4"),
               ),
-              // height: 150,
-              child: Row(
+              title: KSubtitle(
+                text: "Shreyans Jain",
+              ),
+              // isThreeLine: true,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(color: colorCodes[index % 3], width: 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              subtitle: Row(
                 children: [
-                  SizedBox(
-                    width: 20,
+                  Icon(
+                    Icons.follow_the_signs,
+                    size: 20,
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    height: 75,
-                    width: 75,
-                    child: Image.network(
-                        "https://avatars.githubusercontent.com/u/50544190?v=4"),
+                  SizedBox(width: 10),
+                  KSubtitle(text: "38 Followers", size: 15),
+                  SizedBox(width: 15),
+                  Icon(
+                    Icons.follow_the_signs,
+                    size: 20,
                   ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      KSubtitle(text: "Shreyans"),
-                      SizedBox(
-                        height: 3,
-                      ),
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            maxRadius: 15,
-                            child: Icon(
-                              Icons.follow_the_signs,
-                              size: 20,
-                            ),
-                            backgroundColor: Colors.white54,
-                            // maxRadius: ,
-                          ),
-                          SizedBox(width: 10),
-                          KSubtitle(text: "38", size: 15),
-                          SizedBox(width: 15),
-                          CircleAvatar(
-                            maxRadius: 15,
-                            child: Icon(
-                              Icons.follow_the_signs,
-                              size: 20,
-                            ),
-                            backgroundColor: Colors.white54,
-                            // maxRadius: ,
-                          ),
-                          SizedBox(width: 10),
-                          KSubtitle(text: "33", size: 15),
-                        ],
-                      ),
-                    ],
-                  )
+                  SizedBox(width: 10),
+                  KSubtitle(text: "33 Following", size: 15),
                 ],
               ),
             );
