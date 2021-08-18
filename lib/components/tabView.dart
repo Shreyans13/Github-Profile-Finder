75fa24c704a05873dbd045439adbd5e7d0193e44
Shreyans_13 <shreyans.jain@juspay.in>
2021-08-19 02:06:05 +0530

    fixed/TabView/TabBackgroundColor

--- lib/components/tabView.dart
+++ lib/components/tabView.dart
@@ -3,6 +3,7 @@ import 'package:github_profile_finder/pages/tabs/followers.dart';
 import 'package:github_profile_finder/pages/tabs/following.dart';
 import 'package:github_profile_finder/pages/tabs/gist.dart';
 import 'package:github_profile_finder/pages/tabs/repository.dart';
+import 'package:github_profile_finder/util/customColors.dart';
 
 class TabViews extends StatefulWidget {
   const TabViews({Key? key}) : super(key: key);
@@ -13,34 +14,48 @@ class TabViews extends StatefulWidget {
 
 class _TabViewsState extends State<TabViews> with TickerProviderStateMixin {
   late TabController _tabController;
-  List<Widget> _tabs = [
-    Tab(text: "Repository"),
-    Tab(text: "Gists"),
-    Tab(text: "Followers"),
-    Tab(text: "Following"),
-    // Tab(text: "Events")
-  ];
-
-  Color getColor(Set<MaterialState> states) {
-    const Set<MaterialState> interactiveStates = <MaterialState>{
-      MaterialState.pressed,
-      MaterialState.hovered,
-      MaterialState.focused,
-    };
-    if (states.any(interactiveStates.contains)) {
-      return Colors.blue;
-    }
-    return Colors.red;
-  }
+  int selectedTab = 0;
+  // int currentTab;
+  late List<Widget> _tabs;
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: _tabs.length, vsync: this);
+
+    _tabController = TabController(length: 4, vsync: this);
+    _tabController.addListener(() {
+      print(_tabController.index);
+      setState(() {
+        selectedTab = _tabController.index;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    _tabs = [
+      Container(
+          color: selectedTab == 0 ? KDarkBlueColor : null,
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          margin: EdgeInsets.zero,
+          child: Tab(text: "Repository")),
+      Container(
+        color: selectedTab == 1 ? KDarkYellowColor : null,
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Tab(text: "Gists"),
+      ),
+      Container(
+        color: selectedTab == 2 ? KDarkGreenColor : null,
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Tab(text: "Followers"),
+      ),
+      Container(
+        color: selectedTab == 3 ? KDarkBlueColor : null,
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Tab(text: "Following"),
+      ),
+      // Tab(text: "Events")
+    ];
     return Column(
       children: [
         TabBar(
@@ -48,10 +63,9 @@ class _TabViewsState extends State<TabViews> with TickerProviderStateMixin {
           controller: _tabController,
           labelColor: Colors.black,
           tabs: _tabs,
-          // unselectedLabelColor: Colors.red,
-          overlayColor: MaterialStateProperty.all(Colors.green),
-          // automaticIndicatorColorAdjustment: true,
-          // indicatorColor: Colors.green,
+          indicatorSize: TabBarIndicatorSize.label,
+          indicatorColor: Colors.transparent,
+          overlayColor: MaterialStateProperty.all(Colors.transparent),
         ),
         Expanded(
           child: TabBarView(
