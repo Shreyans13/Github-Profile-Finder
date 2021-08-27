7cf8c3c220b8082de650b41d70d0c3d46dd08128
Shreyans_13 <shreyans.jain@juspay.in>
2021-08-27 09:37:09 +0530

    fix/bug/searchOverflow

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -28,89 +28,101 @@ class _SearchPageState extends State<SearchPage> {
 
   @override
   Widget build(BuildContext context) {
+    final _bottom = MediaQuery.of(context).viewInsets.bottom;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       // resizeToAvoidBottomInset: false,
+      // resizeToAvoidBottomPadding: false,
       backgroundColor: KDarkGreenColor,
       body: SafeArea(
-        child: Container(
-            alignment: Alignment.center,
-            color: KDarkGreenColor,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  flex: 3,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: AnimatedSwitcher(
-                      duration: const Duration(seconds: 1),
+        child: SingleChildScrollView(
+          reverse: true,
+          // scrollDirection: Axis.vertical,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: _bottom),
+            child: Container(
+                height: MediaQuery.of(context).size.height,
+                alignment: Alignment.center,
+                color: KDarkGreenColor,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: AnimatedSwitcher(
+                          duration: const Duration(seconds: 1),
+                          child: Container(
+                            child: AnimatedImage(),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
                       child: Container(
-                        height: 300,
-                        child: AnimatedImage(),
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.vertical(top: Radius.circular(35)),
+                          color: Colors.white70,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(32.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              KTitle(text: "Search", size: 25),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              KSubtitle(
+                                  text:
+                                      "Find your Github Profile and share it with one click"),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              TextField(
+                                  controller: _controller,
+                                  decoration: InputDecoration(
+                                    fillColor: KDarkGreenColor,
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Enter Github ID',
+                                  )),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Center(
+                                child: SizedBox(
+                                  width: double.infinity,
+                                  child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          //     shape: CircleBorder(),
+                                          primary: KDarkOrangeColor),
+                                      onPressed: () => {
+                                            Navigator.of(context).push(
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        Home())),
+                                            getUtility().initiateApi(
+                                                _controller.value.text)
+                                          },
+                                      child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child:
+                                              FaIcon(FontAwesomeIcons.search))),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius:
-                          BorderRadius.vertical(top: Radius.circular(35)),
-                      color: Colors.white70,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(32.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          KTitle(text: "Search", size: 25),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          KSubtitle(
-                              text:
-                                  "Find your Github Profile and share it with one click"),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          TextField(
-                              controller: _controller,
-                              decoration: InputDecoration(
-                                fillColor: KDarkGreenColor,
-                                border: OutlineInputBorder(),
-                                hintText: 'Enter Github ID',
-                              )),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Center(
-                            child: SizedBox(
-                              width: double.infinity,
-                              child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                      //     shape: CircleBorder(),
-                                      primary: KDarkOrangeColor),
-                                  onPressed: () => {
-                                        Navigator.of(context).push(
-                                            MaterialPageRoute(
-                                                builder: (context) => Home())),
-                                        getUtility()
-                                            .initiateApi(_controller.value.text)
-                                      },
-                                  child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: FaIcon(FontAwesomeIcons.search))),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            )),
+                    )
+                  ],
+                )),
+          ),
+        ),
       ),
     );
   }
